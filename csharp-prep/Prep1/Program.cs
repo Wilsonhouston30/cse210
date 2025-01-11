3747beba1640c72c5c02acb07a40c741bf5da58b
Wilson Houston <wilsonhouston@Wilsons-MacBook-Air.local>
2025-01-10 22:41:37 -0700

    PracticedInputAndOutput

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? "); // display's message requesting name
+        string firstName = Console.ReadLine(); // saves input into the variable firstName
+
+        Console.Write("What is your last name?");
+        string lastName = Console.ReadLine();
+
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName} "); // formated the string to have set places for variables recorded
     }
 }
