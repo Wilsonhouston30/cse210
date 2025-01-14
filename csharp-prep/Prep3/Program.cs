922e24ad15a3bf5af36f6a3239a73f18fd66a563
Wilson Houston <wilsonhouston@Wilsons-MacBook-Air.local>
2025-01-13 18:52:18 -0700

    Completed loops prep

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random(); // Random class
+
+        int magicNumber = random.Next(1, 20); // setting the magic number
+
+
+        Console.WriteLine("Enter a number between 1 and 20");
+        int userNumber = Int32.Parse(Console.ReadLine());
+
+        while (userNumber != magicNumber)
+        {
+            if (userNumber < magicNumber)
+            {
+                System.Console.WriteLine("Higher");
+                Console.Write("Try again: ");
+                userNumber = Int32.Parse(Console.ReadLine());
+            }
+            else
+            {
+                System.Console.WriteLine("Lower");
+                Console.Write("Try again: ");
+                userNumber = Int32.Parse(Console.ReadLine());
+            }
+        }
+
+        System.Console.WriteLine(($"The number was {magicNumber}, you did it!!"));
+
     }
 }
